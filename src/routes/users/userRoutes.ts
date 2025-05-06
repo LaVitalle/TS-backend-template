8ed44e277482a6954f7983c9a94d5a30e5787b31
arthurfelipe3252 <arthurfelipebalmantdesouza2019@gmail.com>
2025-05-06 19:29:10 -0300

    Token não testado

--- src/routes/users/userRoutes.ts
+++ src/routes/users/userRoutes.ts
@@ -1,10 +1,8 @@
 import { Router, Request, Response } from "express";
 import UserService from "../../services/UserService";
-import LoginService from "../../services/LoginService";
 
 const usrRoute = Router();
 const userService = new UserService();
-const loginService = new LoginService();
 
 usrRoute.post("/create", (req: Request, res: Response) => {
   try {
@@ -16,7 +14,7 @@ usrRoute.post("/create", (req: Request, res: Response) => {
 
 usrRoute.post("/login", (req: Request, res: Response) => {
     try {
-      loginService.ValidateLogin(req, res);
+      userService.ValidateLogin(req, res);
       } catch (error) {
         res.status(500).json({ message: ("Error" + error) });
       } 
