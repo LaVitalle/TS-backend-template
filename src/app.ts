8b42d8876d67cd12fdce97a9d0fad87d371e0d1e
lavitalle <vitorhugoinacio@outlook.com>
2025-05-09 19:54:48 -0300

    fix:Restoring things

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import allRoutes from './routes/appRoutes';
+import cookieParser from "cookie-parser";
 
 const app = express();
 const routes = allRoutes;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/', routes);
 
 export default app;
